f5b96cc15143e7b5662f323bd4800f4a977ba123
brainwaves-crazycoders <sharma.sukoon4@gmail.com>
2014-11-09 02:57:26 +0530

    gitu

--- egift-android/src/com/example/sg/Lent.java
+++ egift-android/src/com/example/sg/Lent.java
@@ -16,141 +16,190 @@ import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
-import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.os.StrictMode;
+import android.telephony.gsm.SmsManager;
 import android.util.Log;
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
-@SuppressLint("NewApi")
 public class Lent extends Activity {
+	Button send;
+	EditText et1,et2,et3;
 	String result = null;
 	InputStream is = null;
-	Button sendc;
+String resValue;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.alent);
-		
-		StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-
-		StrictMode.setThreadPolicy(policy); 
-		
-		sendc=(Button)findViewById(R.id.sendc);
-		sendc.setOnClickListener(new OnClickListener() {
+		send=(Button)findViewById(R.id.button1);
+		et1 = (EditText) findViewById(R.id.editText3);
+		et2 = (EditText) findViewById(R.id.editText2);
+		et3= (EditText) findViewById(R.id.editText1);
+		send.setOnClickListener(new View.OnClickListener() {
 			
 			@Override
-			public void onClick(View arg0) {
+			public void onClick(View v) {
 				// TODO Auto-generated method stub
+				String number = et1.getText().toString();
+				String message="This is the message";
+				String add=et2.getText().toString();
 				send();
+					if(resValue=="error authenticating")
+					{
+						Toast.makeText(getApplicationContext(), "error authenticating", Toast.LENGTH_SHORT).show();
+					}
+					else if(resValue=="success")
+					{
+						Toast.makeText(getApplicationContext(), "success", Toast.LENGTH_SHORT).show();
+					}
+					else if(resValue=="Low Balance")
+					{
+						Toast.makeText(getApplicationContext(), "Low Balance", Toast.LENGTH_SHORT).show();
+					}
+				sendSMS(number,message);
+				sendEmail(add);
 			}
 		});
-	}
-	
-	void send(){
-		try {
-			 HttpClient httpclient = new DefaultHttpClient();
-//		    HttpPost httppost = new HttpPost("http://192.168.0.199/egift/send.php");
-		    HttpPost httppost = new HttpPost("http://192.168.0.199/testj.json");
-		   System.out.println("connect to website first time");
-		     // Add your data
-	        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(5);
-	            String acc = "1";
-				nameValuePairs.add(new BasicNameValuePair("acc", acc));
-	            String cname = "ffff";
-				nameValuePairs.add(new BasicNameValuePair("cname", cname));
-	            String uname = "gautham";
-				nameValuePairs.add(new BasicNameValuePair("uname", uname));
-	            String pass = "44";
-				nameValuePairs.add(new BasicNameValuePair("pass", pass));
-	            String amount = "10";
-				nameValuePairs.add(new BasicNameValuePair("amount", amount));
-				String email = "email";
-				nameValuePairs.add(new BasicNameValuePair("email", email));
-				String phno = "233";
-				nameValuePairs.add(new BasicNameValuePair("phno", phno));
-				String time = "2014:23:12 12:12:12";
-				nameValuePairs.add(new BasicNameValuePair("time", time));
-				
-	           httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
-       // Execute HTTP Post Request
-	        HttpResponse response = httpclient.execute(httppost);
-        
-	       HttpEntity entity = response.getEntity();
-
-   	      is = entity.getContent();
-   	    System.out.println("..............");
-	    } catch (ClientProtocolException e) {
-	        // TODO Auto-generated catch block
-	    } catch (IOException e) {
-	        // TODO Auto-generated catch block
-	    }
-    
-
-		download();
-try{
-	System.out.println("inside try catch1");
-	System.out.println("res="+result);
-	//result="{\"a\":\"success\"}";
-	//{"a":"success"}
-	JSONObject json_data=new JSONObject(result);
-	System.out.print("ll");
-//JSONArray jArray = new JSONArray(result);
-System.out.println("inside try catch2");
-Toast.makeText(getApplicationContext(), json_data.getString("a"), 
-		   Toast.LENGTH_LONG).show();
-	int k=0;
- //int y = jArray.length();
-// System.out.println("y="+y);
-
-	System.out.println("inside try catch3");
-	
-//JSONObject js = jArray.getJSONObject(0);
-System.out.println("inside try catch 3.5");
-
-System.out.println(json_data.getString("a"));
-
-
-System.out.println("inside try catch4");
-	
-
-
-} catch (JSONException e) {System.out.println("caught here");
-
-}
-
-	}
-	void download()
-	{ System.out.println("in download");
 		
-		try{
-			
-				BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),8);
-			       StringBuilder sb = new StringBuilder();
-		           String line = null;
-		           int y=0;
-		           while ((line = reader.readLine()) != null&&y==0) {
-		                   sb.append(line + "\n");y=1;
-
-		           }
-		        
-		           is.close();
-		           result  = sb.toString();
-		          
-		           System.out.println("end of first half");
-		   }catch(Exception e){
-		           Log.e("log_tag", "Error converting result "+e.toString());
-		           System.out.println("caught in download");
-		           
-		   }
+		
 	}
+	
+	public void sendSMS(String number, String message){
+		
+		SmsManager sms = SmsManager.getDefault();
+		sms.sendTextMessage(number, null, message, null, null);
+	}
+	
+	 protected void sendEmail(String add) {
+	      Log.i("Send email", "");
+
+	      String[] TO = {add};
+	     // String[] CC = {"mcmohd@gmail.co"};
+	      Intent emailIntent = new Intent(Intent.ACTION_SEND);
+	      emailIntent.setData(Uri.parse("mailto:"));
+	      emailIntent.setType("text/plain");
+
+
+	      emailIntent.putExtra(Intent.EXTRA_EMAIL, TO);
+	      //emailIntent.putExtra(Intent.EXTRA_CC, CC);
+	      emailIntent.putExtra(Intent.EXTRA_SUBJECT, "Your subject");
+	      emailIntent.putExtra(Intent.EXTRA_TEXT, "Email message goes here");
+
+	      try {
+	         startActivity(Intent.createChooser(emailIntent, "Send mail..."));
+	         finish();
+	         Log.i("Finished sending email...", "");
+	      } catch (android.content.ActivityNotFoundException ex) {
+	         Toast.makeText(Lent.this, 
+	         "There is no email client installed.", Toast.LENGTH_SHORT).show();
+	      }
+	   }
+	 void send(){
+			try {
+				 HttpClient httpclient = new DefaultHttpClient();
+			    HttpPost httppost = new HttpPost("http://192.168.0.199/egift/send.php");
+			    //HttpPost httppost = new HttpPost("http://192.168.0.199/testj.json");
+			   System.out.println("connect to website first time");
+			     // Add your data
+		        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(5);
+		            String acc = "1";
+					nameValuePairs.add(new BasicNameValuePair("acc", acc));
+		            String cname = "ffff";
+					nameValuePairs.add(new BasicNameValuePair("cname", cname));
+		            String uname = "gautham";
+					nameValuePairs.add(new BasicNameValuePair("uname", uname));
+		            String pass = "1";
+					nameValuePairs.add(new BasicNameValuePair("pass", pass));
+		            String amount = et3.getText().toString();;
+					nameValuePairs.add(new BasicNameValuePair("amount", amount));
+					String email = et2.getText().toString();;
+					nameValuePairs.add(new BasicNameValuePair("email", email));
+					String phno = et3.getText().toString();;
+					nameValuePairs.add(new BasicNameValuePair("phno", phno));
+					String time = "2014:23:12 12:12:12";
+					nameValuePairs.add(new BasicNameValuePair("time", time));
+					
+		           httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+	       // Execute HTTP Post Request
+		        HttpResponse response = httpclient.execute(httppost);
+	        
+		       HttpEntity entity = response.getEntity();
+
+	   	      is = entity.getContent();
+	   	    System.out.println("..............");
+		    } catch (ClientProtocolException e) {
+		        // TODO Auto-generated catch block
+		    } catch (IOException e) {
+		        // TODO Auto-generated catch block
+		    }
+	    
+
+			download();
+	try{
+		System.out.println("inside try catch1");
+		System.out.println("res="+result);
+		//result="{\"a\":\"success\"}";
+		//{"a":"success"}
+		JSONObject json_data=new JSONObject(result);
+		System.out.print("ll");
+	//JSONArray jArray = new JSONArray(result);
+	System.out.println("inside try catch2");
+	Toast.makeText(getApplicationContext(), json_data.getString("a"), 
+			   Toast.LENGTH_LONG).show();
+		int k=0;
+	 //int y = jArray.length();
+	// System.out.println("y="+y);
+
+		System.out.println("inside try catch3");
+		
+	//JSONObject js = jArray.getJSONObject(0);
+	System.out.println("inside try catch 3.5");
+
+	System.out.println(json_data.getString("a"));
+	resValue=json_data.getString("a");
+
+
+	System.out.println("inside try catch4");
+		
+
+
+	} catch (JSONException e) {System.out.println("caught here");
+
+	}
+
+		}
+		void download()
+		{ System.out.println("in download");
+			
+			try{
+				
+					BufferedReader reader = new BufferedReader(new InputStreamReader(is,"iso-8859-1"),8);
+				       StringBuilder sb = new StringBuilder();
+			           String line = null;
+			           int y=0;
+			           while ((line = reader.readLine()) != null&&y==0) {
+			                   sb.append(line + "\n");y=1;
+
+			           }
+			        
+			           is.close();
+			           result  = sb.toString();
+			          
+			           System.out.println("end of first half");
+			   }catch(Exception e){
+			           Log.e("log_tag", "Error converting result "+e.toString());
+			           System.out.println("caught in download");
+			           
+			   }
+		}
 }
